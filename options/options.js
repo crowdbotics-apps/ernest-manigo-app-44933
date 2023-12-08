41fe44035eb82183eb379984f413c53f1b390637
Crowdbotics <team@crowdbotics.com>
2023-12-08 23:58:07 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ernest-manigo-app-44933",
+  url: "https://ernest-manigo-app-44933.botics.co",
+  api: "https://ernest-manigo-app-44933.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
